6a9028f8d2cb15faaa3c82308c121cb83b7aadb8
Bahaa Youssof <bahaayoussof@gmail.com>
2021-06-03 11:31:58 +0200

    Single Page Applications Part 2

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -24,23 +24,21 @@ class Header extends Component {
             <>
                 <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarBrand className="mr-auto" href="/">
-                            <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
-                        </NavbarBrand>
-
+                        <NavbarToggler className="mr-3" onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="/"><img src='../assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                                 <NavItem>
-                                    <NavLink className="nav-link" to='/home'> <span className="fa fa-home fa-lg"></span> Home</NavLink>
+                                    <NavLink className="nav-link" to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to='/aboutus'> <span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                                    <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to='/menu'> <span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                                    <NavLink className="nav-link" to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to='/contactus'> <span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                                    <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                                 </NavItem>
                             </Nav>
                         </Collapse>
